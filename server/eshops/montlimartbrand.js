c8a6c97fca4a14ef4d4c0242a8f14e3687255bc2
FLORIAN LIGONNIERE <florian.ligonniere@edu.devinci.fr>
2023-03-16 15:18:47 +0100

    Created scraping file for montlimart

---
+++ server/eshops/montlimartbrand.js
@@ -0,0 +1,52 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.products-list__block')
+    .map((i, element) => {
+      const name = $(element)
+        .find('.product-miniature__title a')
+        .text()
+        .trim();
+      const price = parseInt(
+        $(element)
+          .find('.product-miniature__pricing .price')
+          .text()
+      );
+
+      return {name, price};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+    try {
+      const response = await fetch(url);
+  
+      if (response.ok) {
+        console.log('test');
+        const body = await response.text();
+  
+        return parse(body);
+      }
+  
+      console.error(response);
+  
+      return null;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const montlimartbrand = require('./eshops/montlimartbrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop = 'https://www.montlimart.com/72-nouveautes') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await montlimartbrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
