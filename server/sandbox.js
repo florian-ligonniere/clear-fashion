8224e16cac7b5e6b4385eac9c44530a97bf7a8ec
FLORIAN LIGONNIERE <florian.ligonniere@edu.devinci.fr>
2023-03-16 15:43:15 +0100

    Created scrapping file for Circle Sportswear

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
 const montlimartbrand = require('./eshops/montlimartbrand');
+const circleSportswearbdrand = require('./eshops/circleSportswearbdrand');
 
-async function sandbox (eshop = 'https://www.montlimart.com/72-nouveautes') {
+async function sandbox (eshop = 'https://shop.circlesportswear.com/collections/collection-femme') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
-    const products = await montlimartbrand.scrape(eshop);
+    const products = await circleSportswearbdrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
